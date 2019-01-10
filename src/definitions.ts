56a86e79497b6996803429918216bc216a72be74
teroneko <giteroneko@gmail.com>
2019-01-10 14:09:59 +0000

    PromiseFunction type defintions corrected;

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,9 @@
 export const nameof = <T>(name: Extract<keyof T, string>): string => name;
 
-export type PromiseFunction = (...args: any[]) => Promise<any>;
-export type PromiseFunction1<T> = (args: T) => Promise<any>;
+export type PromiseFunction = () => Promise<any>;
+export type PromiseFunction1Return<ArgumentType, ReturnType> = (arg: ArgumentType) => Promise<ReturnType>;
+export type PromiseFunctionRestArray = (...args: any[]) => Promise<any>;
+export type PromiseFunctionRestArrayReturn<R> = (...args: any[]) => Promise<R>;
 
 export type GetFunctionResult<F extends Function> = F extends (...args: any[]) => infer A ? A : never;
 export type GetPromiseFunctionGenericType<F extends Function> = GetFunctionResult<F> extends Promise<infer T> ? T : never;
