fbbe32fd8cd4416c7f002eec61078d03ecad6eab
teroneko <giteroneko@gmail.com>
2019-01-15 08:42:14 +0000

    Type FunctionParameterAt added

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,5 +5,6 @@ export type T1TResultPromiseFunction<T1, TResult> = (arg: T1) => Promise<TResult
 export type RestArrayTResultPomiseFunction<TResult> = (...args: any[]) => Promise<TResult>;
 export type PromiseFunctionGenericType<TFunction extends RestArrayPromiseFunction> = ReturnType<TFunction> extends Promise<infer T> ? T : never;
 export type TResultFunction<F extends RestArrayFunction, TResult> = F extends (...args: infer TParams) => any ? (...args: TParams) => TResult : never;
+export type FunctionParameterAt<F, TIndex extends number> = F extends (...args: infer TParams) => any ? TParams[TIndex] : never;
 
 export const nameof = <T>(name: Extract<keyof T, string>): string => name;
