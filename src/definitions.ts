d26f24db1117e889bf4ee29669e983aadbf50baa
teroneko <giteroneko@gmail.com>
2019-01-21 07:09:16 +0000

    names has been changed; new types added;

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,11 +1,19 @@
-export type RestArrayFunction = (...args: any[]) => any;
-export type EmptyPromiseFunction = () => Promise<any>;
-export type RestArrayPromiseFunction = (...args: any[]) => Promise<any>;
-export type T1TResultPromiseFunction<T1, TResult> = (arg: T1) => Promise<TResult>;
-export type RestArrayTResultPomiseFunction<TResult> = (...args: any[]) => Promise<TResult>;
-export type PromiseFunctionGenericType<TFunction extends RestArrayPromiseFunction> = ReturnType<TFunction> extends Promise<infer T> ? T : never;
-export type TResultFunction<F extends RestArrayFunction, TResult> = F extends (...args: infer TParams) => any ? (...args: TParams) => TResult : never;
-export type FunctionParameterAt<F, TIndex extends number> = F extends (...args: infer TParams) => any ? TParams[TIndex] : never;
-export type ThisContext<T extends Function> = T extends (this: infer T) => any ? T : never;
+export type RestArrayFn = (...args: any[]) => any;
+export type FunctionResultUnion<F extends RestArrayFn, Result> = F extends (this: infer Context, ...args: infer Params) => any ? (this: Context, ...args: Params) => Result : never;
+
+export type FnParamAt<F, Index extends number> = F extends (...args: infer Params) => any ? Params[Index] : never;
+export type ContextType<T extends Function> = T extends (this: infer T) => any ? T : never;
+
+
+export type EmptyParamsToPromiseFn = () => Promise<any>;
+
+export type RestArrayToPromiseFn = (...args: any[]) => Promise<any>;
+export type RestArrayToPromiseResultFn<Result> = (...args: any[]) => Promise<Result>;
+
+export type ParamToPromiseFn<T1> = (arg: T1) => Promise<any>;
+export type ParamToPromiseResultFn<T1, Result> = (arg: T1) => Promise<Result>;
+
+export type PromiseResultFromFn<F extends RestArrayToPromiseFn> = ReturnType<F> extends Promise<infer T> ? T : never;
+
 
 export const nameof = <T>(name: Extract<keyof T, string>): string => name;
